245d592210379b8a54e4dd4f853cd24466253074
luisaoliveira <luisa.oliveira.vicente@hotmail.com>
2021-03-30 23:28:24 -0300

    Configuração dos astros

--- src/libs/headers/celestial.h
+++ src/libs/headers/celestial.h
@@ -2,8 +2,43 @@
 #define _CELESTIAL_H_
 
 // Source: en.wikipedia.org/wiki/List_of_natural_satellites
-enum planets{mercury = 1, venus, earth, mars, jupiter, saturn, uranus, nepturne};
+enum orbes{mercury = 1, venus, earth, mars, jupiter, saturn, uranus, nepturne};
 enum naturalSatellites{moon = 1, phobos, deimos}; // Only Earth and Mars
 
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#define MAX_LADOS 40
+
+typedef struct{
+    GLint orbitcx;
+    GLint orbitcy;
+    GLint orbitcz;
+    GLint orbitrx;
+    GLint orbitrz;
+    GLint radius;
+    GLfloat velTranslation;
+    GLfloat velRotation;
+    GLint posx;
+    GLint posy;
+    GLint posz;
+    GLfloat restartT;
+} Celestial;
+
+Celestial planets[8];
+Celestial satellites[3];
+Celestial sun;
+
+void buildCelestial(Celestial *cel, int x, int y, int z, int rx, int ry,
+                     int posx, int posy, int posz,
+                     int radius, float rot, float trans);
+void drawCelestials();
+void drawOrbits();
+void moveCelestials();
 
 #endif // _CELESTIAL_H_
+
