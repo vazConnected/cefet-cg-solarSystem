bd94d742163ebba01a47711247c0b3908e2604c2
Pedro Vaz <holo_vaz@outlook.com>
2021-04-04 15:50:41 -0300

    Add comentarios na main e creditos para a texturas no README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Sistema Solar
-Projeto de simulação do Sistema Solar, desenvolvido em C e OpenGL com a biblioteca FreeGLUT.
+Projeto de simulação do Sistema Solar, desenvolvido em C e OpenGL com a biblioteca FreeGLUT para disciplina Computação Gráfica do [Cefet-MG](https://cefetmg.br).
 
 ## Preparando o Ambiente de Execução
 Para compilar e executar o projeto é necessário instalar os seguintes recursos:
@@ -12,4 +12,5 @@ Após instalar os recursos, basta executar o arquivo `cg-solarSystem.sh`, locali
 
 ## Créditos
 - Devs: [Luísa Oliveira](https://github.com/luisaoliveira), [Pedro Vaz](https://github.com/vazConnected) e [Stéphanie Fonseca](https://github.com/steponnie);
-- Prof.: [Glender Brás](https://github.com/glenderbras).
+- Prof.: [Glender Brás](https://github.com/glenderbras);
+- Texturas: [Solar System Scope](https://www.solarsystemscope.com/textures/).
--- src/main.c
+++ src/main.c
@@ -9,10 +9,10 @@
 #include "libs/headers/celestial.h"
 #include "libs/headers/textures.h"
 
-#define USEFUL_WIDTH 1280
-#define USEFUL_HEIGHT 720
-#define STANDARD_EYE_DISTANCE 2100
-#define STANDARD_DEPTH_OF_VIEW 10000
+#define USEFUL_WIDTH 1280 // Largura da area util da simulacao
+#define USEFUL_HEIGHT 720 // Altura da area util da simulacao
+#define STANDARD_EYE_DISTANCE 2100 // Valor para as coordenadas de posicionamento das cameras lateral e superior
+#define STANDARD_DEPTH_OF_VIEW 10000 // Distancia para o alcance de visao da camera 
 
 typedef struct{
 	GLdouble eye[3];
@@ -20,8 +20,8 @@ typedef struct{
  	GLdouble up[3];
 } LookAt;
 
-LookAt lookAt;
-GLfloat fov;
+LookAt lookAt; // Parametros para o gluLookAt
+GLfloat fov; // Angulo para o campo de visao
 
 bool orbit = false;
 bool move = false;
@@ -30,7 +30,6 @@ bool light = true;
 char showPlanet;
 
 void initialize(){
-
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	glEnable(GL_BLEND );
@@ -48,21 +47,24 @@ void initialize(){
 
     showPlanet = - 1;
 
+    // direcao olhada pela camera
 	for(int i = 0; i < 3; i++){
 		lookAt.center[i] = 0;
 	}
-	lookAt.eye[0] = 1500;
-	lookAt.eye[1] = 600;
-	lookAt.eye[2] = 600;
-	lookAt.up[0] = 0;
-	lookAt.up[1] = 1;
-	lookAt.up[2] = 0;
+	lookAt.eye[0] = 1500; // posicao x da camera
+	lookAt.eye[1] = 600;  // posicao y da camera
+	lookAt.eye[2] = 600;  // posicao z da camera
+	lookAt.up[0] = 0; // valor do x do vetor para cima da camera
+	lookAt.up[1] = 1; // valor do y do vetor para cima da camera
+	lookAt.up[2] = 0; // valor do z do vetor para cima da camera
 
-    // build celestials to draw
-    // orbit center x, y and z, orbit radius x and z, position x, y and z, size, rotation e translation
+    // Definicao dos objetos celestiais
+    // buildCelestial: <astro> <{x, y, z} do centro da orbita> <{x, z} do raio da orbita>, <{x, y, z} posicao> <raio> <rotacao> <translacao> <textura>
+
+    // inicializa valores para o sol
     buildCelestial(&sun, 0, 0, 0, 0, 0, 0, 200, 1, 150, 0, "textures/2k_sun.jpg"); // build the sun
 
-    // build the planets
+    // inicializa os valores dos planetas
 	buildCelestial(&planets[mercury - 1],   -50, 200, 1,  270,  210,  220, 200, 1, 20, 0.8, "textures/2k_mercury.jpg");
 	buildCelestial(&planets[venus - 1],     -50, 200, 1,  370,  360,  320, 200, 1, 25, 0.7, "textures/2k_venus_surface.jpg");
 	buildCelestial(&planets[earth - 1],     -40, 200, 1,  490,  510,  450, 200, 1, 40, 0.6, "textures/2k_earth_daymap.jpg");
@@ -72,21 +74,21 @@ void initialize(){
 	buildCelestial(&planets[uranus - 1],    -20, 200, 1, 1260, 1170, 1240, 200, 1, 60, 0.2, "textures/2k_uranus.jpg");
 	buildCelestial(&planets[nepturne - 1], -150, 200, 1, 1530, 1350, 1380, 200, 1, 50, 0.1, "textures/2k_neptune.jpg");
 
-	// build earth's atmosphere
+	// carrega a textura da atmosfera da Terra
 	atmosphere = carregaTextura("textures/2k_earth_clouds.png");
 
-    // build the satellites
+    // inicializa satelites naturais
 	buildCelestial(&satellites[moon - 1],   0, 0, 0, 60, 60, 510, 230, 1, 10, 0.4, "textures/2k_moon.jpg");
 	buildCelestial(&satellites[phobos - 1], 0, 0, 0, 50, 50, 650, 220, 1,  5, 0.3, "textures/2k_eris_fictional.jpg");
 	buildCelestial(&satellites[deimos - 1], 0, 0, 0, 50, 50, 650, 190, 1,  5, 0.3, "textures/2k_makemake_fictional.jpg");
-
-    // Os valores foram aleatorios (tentativa e erro) muito chato ter que ficar trocando
 }
 
+// razao de aspecto da area "desenhavel" da tela
 GLdouble getUsefulAreaRatio(){
 	return (GLdouble)USEFUL_WIDTH / (GLdouble)USEFUL_HEIGHT;
 }
 
+// razao de aspecto da janela
 GLdouble getWindowRatio(){
 	return (GLdouble)glutGet(GLUT_WINDOW_WIDTH) / (GLdouble)glutGet(GLUT_WINDOW_HEIGHT);
 }
@@ -95,11 +97,13 @@ void camera(){
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
+    // gluPerspective: fov, razaoDeAspecto, zProximo, zDistante
 	gluPerspective(fov, getUsefulAreaRatio(), 1, STANDARD_DEPTH_OF_VIEW);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
+    // gluLookAt; <{x, y, z} posicao da camera> <{x, y, z} direcao que a camera olha> <{x, y, z} direcao e sentido do vetor para cima>
 	gluLookAt(lookAt.eye[0], lookAt.eye[1], lookAt.eye[2], lookAt.center[0], lookAt.center[1], lookAt.center[2], lookAt.up[0], lookAt.up[1], lookAt.up[2]);
 }
 
@@ -114,15 +118,15 @@ void illumination(){
         glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, especularidade);
         glMateriali(GL_FRONT_AND_BACK, GL_SHININESS, especMaterial);
 
-        GLfloat luzAmbiente[4] = {0.2, 0.2, 0.2, 1.0};
+        GLfloat luzAmbiente[4] = {0.02, 0.02, 0.02, 1.0};
         glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzAmbiente);
 
-        // ambient light
+        // Luz ambiente
         glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente);
         glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa);
         glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular);
 
-        // sun light
+        // Sol como fonte de luz
         GLfloat lightPosition[4] = {sun.posx, sun.posy, sun.posz, 1.0};
         GLfloat sunLight[4] = {0.7, 0.6, 0.0, 1.0};
         glLightfv(GL_LIGHT1, GL_AMBIENT, luzAmbiente);
@@ -136,12 +140,14 @@ void callback_draw(){
 
     drawCelestials();
 
+    // Permite que orbitas sejam desenhadas
     if(orbit){
-		drawOrbits(); // draw orbits only if desire
+		drawOrbits();
 	}
 
+    // Permite a movimentacao dos corpos celestes
     if(move){
-		moveCelestials(); // move celestials only if desire
+		moveCelestials();
 	}
 
 	if(light){
@@ -152,13 +158,14 @@ void callback_draw(){
 
     illumination();
 
+    // Definicao dos calores de camera focadas para os corpos celestes
     if(showPlanet > 0 && showPlanet < nepturne){
-        lookAt.eye[0] = planets[showPlanet - 1].posx - 2*planets[showPlanet - 1].radius;
-        lookAt.eye[1] = planets[showPlanet - 1].posy + 100;
-        lookAt.eye[2] = planets[showPlanet - 1].posz - planets[showPlanet - 1].radius;
-        lookAt.center[0] = planets[showPlanet - 1].posx;
-        lookAt.center[1] = planets[showPlanet - 1].posy;
-        lookAt.center[2] = planets[showPlanet - 1].posz;
+        lookAt.eye[0] = planets[showPlanet - 1].posx - 2*planets[showPlanet - 1].radius; // posicao x da camera
+        lookAt.eye[1] = planets[showPlanet - 1].posy + 100; // posicao y da camera
+        lookAt.eye[2] = planets[showPlanet - 1].posz - planets[showPlanet - 1].radius; // posicao z da camera
+        lookAt.center[0] = planets[showPlanet - 1].posx; // x da direcao olhada pela camera
+        lookAt.center[1] = planets[showPlanet - 1].posy; // y da direcao olhada pela camera
+        lookAt.center[2] = planets[showPlanet - 1].posz; // z da direcao olhada pela camera
     }else if(showPlanet == nepturne){
         float angulo = 2 * M_PI * planets[showPlanet - 1].restartT / MAX_LADOS;
         lookAt.eye[0] = planets[showPlanet - 1].posx - 300;
@@ -182,15 +189,17 @@ void callback_draw(){
 void callback_reshape(GLsizei width, GLsizei height){
 	glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glFrustum(0, USEFUL_WIDTH, 0, USEFUL_HEIGHT, 1, STANDARD_DEPTH_OF_VIEW); // left, right, bottom, top, nearVal, farVal
 
-    if(getWindowRatio() == getUsefulAreaRatio()){
+    // glFrustum: left, right, bottom, top, nearVal, farVal
+    glFrustum(0, USEFUL_WIDTH, 0, USEFUL_HEIGHT, 1, STANDARD_DEPTH_OF_VIEW);
+
+    if(getWindowRatio() == getUsefulAreaRatio()){ // razao de aspecto da janela e a mesma da do desenhada
         glViewport(0, 0, width, height);
-    }else if(getWindowRatio() < getUsefulAreaRatio()){
+    }else if(getWindowRatio() < getUsefulAreaRatio()){ // janela mais fina do que deveria
         GLfloat heightViewport = width / getUsefulAreaRatio();
         GLfloat yViewport = (height - heightViewport)/2;
         glViewport(0, yViewport, width, heightViewport);
-    }else if (getWindowRatio() > getUsefulAreaRatio()){
+    }else if (getWindowRatio() > getUsefulAreaRatio()){ // janela mais larga do que deveria
         GLfloat widthViewport = ((GLfloat)height) * getUsefulAreaRatio();
         GLfloat xViewport = (width - widthViewport) / 2;
         glViewport(xViewport, 0, widthViewport, height);
@@ -205,7 +214,7 @@ void callback_reshape(GLsizei width, GLsizei height){
 void callback_keyboardDownFunc(unsigned char key, GLint x, GLint y){
 
 	switch(key){
-		case('1'): // top vision
+		case('1'): // visao superior
 			lookAt.eye[0] = 0;
 			lookAt.eye[1] = STANDARD_EYE_DISTANCE;
 			lookAt.eye[2] = 0.01;
@@ -214,7 +223,7 @@ void callback_keyboardDownFunc(unsigned char key, GLint x, GLint y){
             lookAt.center[2] = 0;
 			showPlanet = -1;
 		break;
-		case('2'): // side view
+		case('2'): // visao lateral
 			lookAt.eye[0] = 0;
 			lookAt.eye[1] = 0;
 			lookAt.eye[2] = STANDARD_EYE_DISTANCE;
@@ -223,12 +232,14 @@ void callback_keyboardDownFunc(unsigned char key, GLint x, GLint y){
             lookAt.center[2] = 0;
 			showPlanet = -1;
 		break;
-		case(' '): // planet view
+		case(' '): // visualizacao com proxidade dos corpos celestes
 			showPlanet++;
-			if(showPlanet > nepturne) showPlanet = 0;
+			if(showPlanet > nepturne){
+                showPlanet = 0;
+            } 
 		break;
 		case('r'):
-		case('R'): // lookAt test
+		case('R'): // visao cinematografica do sistema solar
 			lookAt.eye[0] = 1500;
 			lookAt.eye[1] = 600;
 			lookAt.eye[2] = 600;
@@ -238,18 +249,18 @@ void callback_keyboardDownFunc(unsigned char key, GLint x, GLint y){
 			showPlanet = -1;
 		break;
 		case('o'):
-        case('O'): // request to draw (or not) orbits
+        case('O'): // visualizacao das orbitas ao redor do Sol
             orbit = !orbit;
         break;
         case('m'):
-        case('M'): // request to move (or not) the celestials
+        case('M'): // movimentacao dos corpos celestiais
             move = !move;
         break;
         case('l'):
-        case('L'): // request to move (or not) the celestials
+        case('L'): // iluminacao do sistema solar
             light = !light;
         break;
-        case(27):
+        case(27): // fechar simulacao
             exit(0);
         break;
 	}
@@ -274,7 +285,7 @@ int main(int argc, char** argv){
 	glutReshapeFunc(callback_reshape);
 	glutKeyboardFunc(callback_keyboardDownFunc);
 
-	glutTimerFunc(0, callback_timerFunc, 60);
+	glutTimerFunc(0, callback_timerFunc, 60); // 60 FPS
 
 	initialize();
 
